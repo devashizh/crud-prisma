18ce6c2a1823b8f5d01944665703d2c7dc3e0433
devashizh <devashizh@gmail.com>
2025-10-03 17:16:49 +0530

    create operation

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+import {prisma} from "./src/config/db.js"
 import express from 'express'
 const app = express();
+
+app.use(express.json())
 
 app.get("/",(req,res) =>{
     res.json({
         message:"Welcome!!!"
     })
+})
+
+app.post("/api/users",async (req,res)=>{
+    const {name,email} = req.body;
+
+    //todo: validation
+  const user=  await prisma.user.create({
+      data:{
+        email,
+        name
+      }
+    })
+    return res.json(user)
 })
 
 
